6f503434f8585ffecb6a4303a9818bc882b33b11
Hoprikoma <md.del@hotmail.fr>
2020-04-17 15:45:48 +0200

    Ajout des polices d'écritures + Modifications sur la page profil

--- Application/src/app/shared/components/profile/profile.component.ts
+++ Application/src/app/shared/components/profile/profile.component.ts
@@ -1,32 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgModule } from '@angular/core';
 import { UserService } from '../../service/user.service';
 import { PublicationService } from './../../service/publication.service';
 import { FriendService } from './../../service/friend.service';
-
+import {NgxTinySliderSettingsInterface, NgxTinySliderModule} from 'ngx-tiny-slider';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: [
+    './profile.component.scss',
+    './../../../app.component.scss'
+  ]
 })
-export class ProfileComponent implements OnInit {
 
+
+export class ProfileComponent implements OnInit{
+
+  //Tiny Slider
+  tinySliderConfig: NgxTinySliderSettingsInterface;
+
+  //Publications
   publication: any;
+  
+  // Friends
   friends: any;
+  friendsNumber: any;
+
+  // Infos
   user: any;
+  userPicture;
+  userName;
+  userBio;
+
 
 
   constructor(private UserService: UserService, private PublicationService: PublicationService, private FriendService: FriendService) { }
 
   ngOnInit() {
     this.getDataProfile()
+
+    this.tinySliderConfig = {
+      arrowKeys: true,
+      autoWidth: true,
+      gutter: 10,
+      controlsText: ['<', '>']
+    };
   }
 
   getDataProfile() {
     let token = 1;
 
     this.UserService.getInfosUser(token).subscribe(response => {
-      this.user = response;
+      this.user = response[0];
       console.log(this.user);
       return this.user;
     });
@@ -40,10 +65,9 @@ export class ProfileComponent implements OnInit {
     this.FriendService.getFriendList(token).subscribe(response => {
       this.friends = response;
       console.log(this.friends);
+      this.friendsNumber = Object.keys(this.friends).length;
       return this.friends;
     });
-
-
   }
 
 }
