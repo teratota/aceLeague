6261c88cb2b89a2af33d4255eb1612dff251d807
teratota <thomas_almeida@hotmail.fr>
2020-04-21 12:34:23 +0200

    modification tiny slide

--- Application/src/app/shared/components/profile/profile.component.ts
+++ Application/src/app/shared/components/profile/profile.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit, NgModule} from '@angular/core';
+import { Component, OnInit, NgModule, ViewChildren, ViewChild, QueryList, ElementRef} from '@angular/core';
 import { UserService } from '../../service/user.service';
 import { PublicationService } from './../../service/publication.service';
 import { FriendService } from './../../service/friend.service';
 import {NgxTinySliderSettingsInterface, NgxTinySliderModule} from 'ngx-tiny-slider';
+import { NgxTinySliderComponent } from 'ngx-tiny-slider/lib/ngx-tiny-slider.component';
 
 
 @Component({
@@ -18,7 +19,19 @@ import {NgxTinySliderSettingsInterface, NgxTinySliderModule} from 'ngx-tiny-slid
 export class ProfileComponent implements OnInit{
 
   //Tiny Slider
+  @ViewChildren('slideList') slideList: QueryList<any>;
+  @ViewChild('ngxSlider', {static: false}) private ngxSlider: ElementRef<NgxTinySliderComponent>;
+
   tinySliderConfig: NgxTinySliderSettingsInterface;
+
+  cards = [
+    {
+      img: 'https://images.unsplash.com/photo-1543248939-ff40856f65d4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=933&q=80'
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1558171813-4c088753af8f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80'
+    }
+  ]
 
   //Publications
   publication: any;
@@ -40,14 +53,28 @@ export class ProfileComponent implements OnInit{
   ngOnInit() {
     this.getDataProfile()  
     this.tinySliderConfig = {
+      waiteForDom: true,
       arrowKeys: true,
       autoWidth: true,
-      gutter: 10,
-      controlsText: ['<', '>'],
+      gutter: 35,
+      mouseDrag: true,
       touch: true,
-      waiteForDom: true
+      controls: false,
+      nav: false,
+      loop: false,
+      rewind:true,
+      // @ts-ignore
+      center: true
     };
-  
+  }
+
+  ngAfterViewInit() {
+    // @ts-ignore
+    this.slideList.changes.subscribe(() =>  this.ngxSlider.domReady.next());
+    if (this.cards != null) {
+      // @ts-ignore
+      this.ngxSlider.domReady.next();
+    }
   }
 
   
